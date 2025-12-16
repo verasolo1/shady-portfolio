cdc58e209e6a81b1f486abf01c887668050c532f
V3RA0x01 <30853881+verasolo1@users.noreply.github.com>
2025-12-16 22:36:01 +0300

    Configure base path for GitHub Pages
    Set base path for deployment on GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Shady-Portifilio/", // 👈 REQUIRED for GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
